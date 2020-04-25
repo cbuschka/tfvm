a8ced3dfdbf518a89c4cc0745a142d5f78cfd2de
conni <cbuschka@gmail.com>
2020-04-25 12:43:34 +0200

    Fixed program name detection in main.

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 	"github.com/cbuschka/tfvm"
 	"os"
+	"path"
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		panic(fmt.Sprintf("Usage: ..."))
-	} else if os.Args[0] == "terraform" {
+	programName := path.Base(os.Args[0])
+	if programName == "terraform" {
 		tfvm.RunTerraformSubCommand(os.Args[1:])
-	} else if os.Args[0] != "terraform" && len(os.Args)>1 && os.Args[1] == "terraform" {
+	} else if programName != "terraform" && len(os.Args)>1 && os.Args[1] == "terraform" {
 		tfvm.RunTerraformSubCommand(os.Args[2:])
 	} else {
 		panic(fmt.Sprintf("Unsupported command: %s", os.Args[1]))
