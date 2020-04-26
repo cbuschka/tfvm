c371c606e5f5a7774adde2982f862202bf46ef86
conni <cbuschka@gmail.com>
2020-04-26 18:57:29 +0200

    Fixed version numbers in test.

--- hashicorp_test.go
+++ hashicorp_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestExtractReleases(t *testing.T) {
 
-	html := "foo <a>terraform_1.0</a> bar\n<a>terraform_2.0abc </a> stuff"
+	html := "foo <a>terraform_1.0.0</a> bar\n<a>terraform_2.0.0-abc </a> stuff"
 
 	releases, err := extractReleases(html)
 	if err != nil {
@@ -15,8 +15,8 @@ func TestExtractReleases(t *testing.T) {
 	}
 
 	assert.Equal(t, 2, len(releases))
-	assert.Equal(t, "1.0", releases[0].version)
-	assert.Equal(t, "https://releases.hashicorp.com/terraform/1.0/terraform_1.0_linux_amd64.zip", releases[0].url)
-	assert.Equal(t, "2.0abc", releases[1].version)
-	assert.Equal(t, "https://releases.hashicorp.com/terraform/2.0abc/terraform_2.0abc_linux_amd64.zip", releases[1].url)
+	assert.Equal(t, "1.0.0", releases[0].version)
+	assert.Equal(t, "https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_linux_amd64.zip", releases[0].url)
+	assert.Equal(t, "2.0.0-abc", releases[1].version)
+	assert.Equal(t, "https://releases.hashicorp.com/terraform/2.0.0-abc/terraform_2.0.0-abc_linux_amd64.zip", releases[1].url)
 }
