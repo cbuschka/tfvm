6b6f0fea7c4158146a2798cba9790dbaa6966b81
conni <cbuschka@gmail.com>
2020-05-01 23:27:19 +0200

    Fixed uptodate check.

--- update_check.go
+++ update_check.go
@@ -4,20 +4,20 @@ import "github.com/tcnksm/go-latest"
 
 type UpdateStatus struct {
 	updateAvailable bool
-	latestVersion string
-	currentVersion string
+	latestVersion   string
+	currentVersion  string
 }
 
 func GetUpdateStatus() *UpdateStatus {
 	githubTag := &latest.GithubTag{
-		Owner: "cbuschka",
-		Repository: "tfvm",
+		Owner:             "cbuschka",
+		Repository:        "tfvm",
 		FixVersionStrFunc: latest.DeleteFrontV(),
 	}
 
 	buildInfo := GetBuildInfo()
 	result, err := latest.Check(githubTag, buildInfo.version)
-	if err == nil || result == nil {
+	if err != nil || result == nil {
 		return &UpdateStatus{updateAvailable: false, latestVersion: buildInfo.version, currentVersion: buildInfo.version}
 	}
 	return &UpdateStatus{updateAvailable: result.Outdated, latestVersion: result.Current, currentVersion: buildInfo.version}
