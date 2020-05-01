949e39ec78bfe4e8a047dd5dce1e8e4fe9f458dc
conni <cbuschka@gmail.com>
2020-05-01 12:36:06 +0200

    Improved usage.

--- help_command.go
+++ help_command.go
@@ -6,17 +6,21 @@ import (
 
 func printUsage() {
 	fmt.Printf(`
-Usage: tfvm <sub command> or terraform <terraform opts*>
+Usage:	tfvm <command>
+	or terraform <terraform command and options>
 
- terraform <terraform opts*> - Run terraform configured via .tfvmrc file.
- list - List terraform versions.
- install - Install terraform version.
- which - Print selected terraform version.
- uptodate - Check for updates.
- help - Print this usage information.
- version - Print tfvm version.
+A terraform version manager.
+
+Commands:
+  terraform	Run terraform configured via .tfvmrc/.terraform-version file.
+		(Run 'terraform help' for more information.)
+  list		List terraform versions.
+  install	Install terraform version.
+  which		Print selected terraform version.
+  uptodate	Check for updates.
+  help		Print this usage information.
+  version	Print tfvm version.
 
 For binaries, issues and source code visit https://github.com/cbuschka/tfvm.
-
 `)
 }
