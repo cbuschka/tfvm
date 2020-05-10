eace16997f796972fa122de348074c208376ca71
conni <cbuschka@gmail.com>
2020-05-10 11:34:12 +0200

    Added repo and author to build info.

--- internal/build/build_info.go
+++ internal/build/build_info.go
@@ -6,6 +6,8 @@ import "strings"
 type BuildInfo struct {
 	Version   string
 	Commitish string
+	Repo      string
+	Author    string
 	BuildTime string
 	OS        string
 	Arch      string
@@ -18,5 +20,11 @@ var buildInfoOs string
 var buildInfoArch string
 
 func GetBuildInfo() BuildInfo {
-	return BuildInfo{Version: buildInfoVersion, Commitish: buildInfoCommitish, BuildTime: strings.Replace(buildInfoBuildTime, "_", " ", -1), Arch: buildInfoArch, OS: buildInfoOs}
+	return BuildInfo{Version: buildInfoVersion,
+		Repo:      "https://github.com/cbuschka/tfvm",
+		Author:    "Cornelius Buschka <cbuschka@gmail.com>, https://github.com/cbuschka",
+		Commitish: buildInfoCommitish,
+		BuildTime: strings.Replace(buildInfoBuildTime, "_", " ", -1),
+		Arch:      buildInfoArch,
+		OS:        buildInfoOs}
 }
