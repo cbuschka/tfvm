b0e5913c414e30d291daae9cafe82e2625df7037
conni <cbuschka@gmail.com>
2020-09-12 19:40:41 +0200

    Changed hashicorp_test to be platform independent.

--- internal/remote/hashicorp_test.go
+++ internal/remote/hashicorp_test.go
@@ -1,7 +1,9 @@
 package remote
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"runtime"
 	"testing"
 )
 
@@ -15,8 +17,12 @@ func TestExtractReleases(t *testing.T) {
 	}
 
 	assert.Equal(t, 2, len(releases))
+
 	assert.Equal(t, "1.0.0", releases[0].String())
-	assert.Equal(t, "https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_linux_amd64.zip", GetUrl(releases[0]))
+	expectedUrl := fmt.Sprintf("https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_%s_%s.zip", runtime.GOOS, runtime.GOARCH);
+	assert.Equal(t, expectedUrl, GetUrl(releases[0]))
+
 	assert.Equal(t, "2.0.0-abc", releases[1].String())
-	assert.Equal(t, "https://releases.hashicorp.com/terraform/2.0.0-abc/terraform_2.0.0-abc_linux_amd64.zip", GetUrl(releases[1]))
+	expectedUrl = fmt.Sprintf("https://releases.hashicorp.com/terraform/2.0.0-abc/terraform_2.0.0-abc_%s_%s.zip", runtime.GOOS, runtime.GOARCH);
+	assert.Equal(t, expectedUrl, GetUrl(releases[1]))
 }
