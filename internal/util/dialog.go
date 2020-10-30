103b4493df056cadf970ea4c0dd14124107fa821
conni <cbuschka@gmail.com>
2020-10-30 20:25:08 +0100

    Added dialog comments.

--- internal/util/dialog.go
+++ internal/util/dialog.go
@@ -5,11 +5,13 @@ import (
 	"os"
 )
 
+// Die prints a message and exists the process with exitCode.
 func Die(exitCode int, format string, a ...interface{}) {
 	fmt.Printf("%s\n", fmt.Sprintf(format, a...))
 	os.Exit(exitCode)
 }
 
+// Print prints a message to console.
 func Print(format string, a ...interface{}) {
 	fmt.Printf("%s\n", fmt.Sprintf(format, a...))
 }
