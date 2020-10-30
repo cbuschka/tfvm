1fe0550517f85ed9e79324a5f9f161256964b7d5
conni <cbuschka@gmail.com>
2020-10-30 20:48:34 +0100

    Formatted code.

--- internal/remote/hashicorp_test.go
+++ internal/remote/hashicorp_test.go
@@ -19,10 +19,10 @@ func TestExtractReleases(t *testing.T) {
 	assert.Equal(t, 2, len(releases))
 
 	assert.Equal(t, "1.0.0", releases[0].String())
-	expectedURL := fmt.Sprintf("https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_%s_%s.zip", runtime.GOOS, runtime.GOARCH);
+	expectedURL := fmt.Sprintf("https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_%s_%s.zip", runtime.GOOS, runtime.GOARCH)
 	assert.Equal(t, expectedURL, GetURL(releases[0]))
 
 	assert.Equal(t, "2.0.0-abc", releases[1].String())
-	expectedURL = fmt.Sprintf("https://releases.hashicorp.com/terraform/2.0.0-abc/terraform_2.0.0-abc_%s_%s.zip", runtime.GOOS, runtime.GOARCH);
+	expectedURL = fmt.Sprintf("https://releases.hashicorp.com/terraform/2.0.0-abc/terraform_2.0.0-abc_%s_%s.zip", runtime.GOOS, runtime.GOARCH)
 	assert.Equal(t, expectedURL, GetURL(releases[1]))
 }
