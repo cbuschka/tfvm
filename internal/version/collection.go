3ed23feae110975e67712346c3c42b04fcf40248
conni <cbuschka@gmail.com>
2020-10-30 21:12:53 +0100

    Added collection comments.

--- internal/version/collection.go
+++ internal/version/collection.go
@@ -1,15 +1,19 @@
 package version
 
+// Collection of terraform versions.
 type Collection []*TerraformVersion
 
+// Len gives length of collection.
 func (v Collection) Len() int {
 	return len(v)
 }
 
+// Less compares two terraform version items.
 func (v Collection) Less(i, j int) bool {
 	return v[i].Version.LessThan(v[j].Version)
 }
 
+// Swap swap two terraform version items.
 func (v Collection) Swap(i, j int) {
 	v[i], v[j] = v[j], v[i]
 }
