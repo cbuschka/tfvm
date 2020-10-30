39acf7ff721b9d7d88841aad1a2f64c692a01468
conni <cbuschka@gmail.com>
2020-10-30 21:20:12 +0100

    Added terraform version comments.

--- internal/version/terraform_version.go
+++ internal/version/terraform_version.go
@@ -7,18 +7,22 @@ import (
 
 const noSuchTerraformReleaseMsg = "no such terraform release"
 
+// IsNoSuchTerraformRelease answer if an error describes an unknown terraform version.
 func IsNoSuchTerraformRelease(err error) bool {
 	return err.Error() == noSuchTerraformReleaseMsg
 }
 
+// NewNoSuchTerraformRelease describes an unknown terraform release version.
 func NewNoSuchTerraformRelease() error {
 	return errors.New(noSuchTerraformReleaseMsg)
 }
 
+// String gives string representation of version.
 func (release *TerraformVersion) String() string {
 	return release.Version.String()
 }
 
+// TerraformVersion is a representation of a terraform version.
 type TerraformVersion struct {
 	Version *version.Version
 }
