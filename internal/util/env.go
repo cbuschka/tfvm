514dac323ebd3ad69a9049409b7a7309ca390d36
conni <cbuschka@gmail.com>
2020-10-30 20:24:09 +0100

    Added comments to env.

--- internal/util/env.go
+++ internal/util/env.go
@@ -2,6 +2,7 @@ package util
 
 import "os"
 
+// GetFirstEnv get the value of the first defined environment variable.
 func GetFirstEnv(names ...string) string {
 	for _, name := range names {
 		value := os.Getenv(name)
