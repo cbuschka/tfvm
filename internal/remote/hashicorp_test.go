761a7478dc691448f31ed33c8bd571ea4408e759
conni <cbuschka@gmail.com>
2021-09-27 10:31:09 +0200

    Formatted hashicorp_test.go.

--- internal/remote/hashicorp_test.go
+++ internal/remote/hashicorp_test.go
@@ -20,9 +20,9 @@ func TestExtractReleases(t *testing.T) {
 
 	assert.Equal(t, "1.0.0", releases[0].String())
 	expectedURL := fmt.Sprintf("https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_%s_%s.zip", "myos", "myarch")
-	assert.Equal(t, expectedURL, GetURL(releases[0], platformPkg.Platform{Os:"myos", Arch:"myarch"}))
+	assert.Equal(t, expectedURL, GetURL(releases[0], platformPkg.Platform{Os: "myos", Arch: "myarch"}))
 
 	assert.Equal(t, "2.0.0-abc", releases[1].String())
 	expectedURL = fmt.Sprintf("https://releases.hashicorp.com/terraform/2.0.0-abc/terraform_2.0.0-abc_%s_%s.zip", "myos", "myarch")
-	assert.Equal(t, expectedURL, GetURL(releases[1], platformPkg.Platform{Os:"myos", Arch:"myarch"}))
+	assert.Equal(t, expectedURL, GetURL(releases[1], platformPkg.Platform{Os: "myos", Arch: "myarch"}))
 }
